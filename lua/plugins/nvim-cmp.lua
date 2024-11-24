2272c769dbd9af9a24b62d3d0b756c206c04c16d
Joey Pettit <pettitbass@gmail.com>
2024-11-23 23:23:38 -0600

    added keymaps and configs

---
+++ lua/plugins/nvim-cmp.lua
@@ -0,0 +1,28 @@
+return {
+  "hrsh7th/nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-Space>"] = cmp.mapping.complete(), -- Trigger completion
+      ["<C-j>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item() -- Move down
+        else
+          fallback()
+        end
+      end, { "i", "c" }), -- Works in Insert and Command-line modes
+
+      ["<C-k>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item() -- Move up
+        else
+          fallback()
+        end
+      end, { "i", "c" }), -- Works in Insert and Command-line modes
+
+      ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Confirm selection
+      ["<Tab>"] = cmp.mapping.confirm({ select = true }), -- Optional: Confirm with Tab
+    })
+  end,
+}
